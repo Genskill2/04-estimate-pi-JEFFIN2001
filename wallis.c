e3fc157fd3aa030bf596f25f157cda17cde75b4a
JEFFIN2001 <74717176+JEFFIN2001@users.noreply.github.com>
2021-06-04 14:00:51 +0530

    Add files via upload

---
+++ wallis.c
@@ -0,0 +1,38 @@
+#include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+float wallis_pi(int);
+
+int main(void) {
+  float pi;
+  for (int i=0; i<5; i++) {
+    pi = wallis_pi(i);
+    if (!(fabs(pi - M_PI) > 0.15)) {
+      printf("Estimate with just %d iterations is %f which is too accurate.\n", i, pi);
+      abort();
+    }
+  }
+
+  for (int i=500; i<3000; i++) {
+    pi = wallis_pi(i);
+    if (!(fabs(pi - M_PI) < 0.01)) {
+      printf("Estimate with even %d iterations is %f which is not accurate enough.\n", i, pi);
+      abort();
+    }
+  }
+}
+
+float wallis_pi(int n)
+{
+  float pi=2;
+  float v1,v2;
+  for(int i=1;i<=n;i++)
+   {
+      v1 = 4*i*i;
+      v2 = v1-1;
+      pi *= v1/v2;
+   }
+   return pi;
+}
